27c5db0185f5a7daa3e43091c316091f6a326558
Josephine <shobande.josephin@gmail.com>
2022-03-30 15:49:36 +0100

    implemented details page along with api integration

--- src/pages/details/details.styled.tsx
+++ src/pages/details/details.styled.tsx
@@ -26,16 +26,37 @@ export const BackButton = styled.button`
 export const CountryDetails = styled.div`
     margin-top: 40px;
 
+    @media(min-width: ${({ theme }) => theme?.mobile?.ipad}){
+        display: flex;
+    }
+
+    img{
+        @media(min-width: ${({ theme }) => theme?.mobile?.ipad}){
+            width: 500px;
+            // height:
+        }
+    }
+
     p{
         font-weight: 600;
         margin-bottom: 20px;
         span{
             font-weight: normal;
         }
+    }  
+`
+
+export const CountryDetailsText = styled.div`
+    @media(min-width: ${({ theme }) => theme?.mobile?.ipad}){
+        margin-left: 100px;
+        width: 40%;
+    }
+    @media(max-width: ${({ theme }) => theme?.mobile?.ipad}){
+        // margin-left: 40px;
     }
     
-   
 `
+
 export const CountryName = styled.p`
     font-weight: 800;
     margin-bottom: 30px;
@@ -43,6 +64,43 @@ export const CountryName = styled.p`
     margin-top: 40px;
 `
 
+export const InfoContainer = styled.div`
+    @media(min-width: ${({ theme }) => theme?.mobile?.ipad}){
+        display: flex;
+        justify-content: space-between;
+    }
+
+`
+
 export const OtherInfo = styled.div`
     margin-top: 40px;
+
+    @media(min-width: ${({ theme }) => theme?.mobile?.ipad}){
+        margin-top: 0;
+    }
+
 `
+
+export const BorderCountries = styled.div`
+    margin-top: 40px;
+
+    @media(min-width: ${({ theme }) => theme?.mobile?.ipad}){
+        display: flex;
+        p{
+            margin-right: 20px;
+        }
+    }
+
+    button{
+        width: 100px;
+        height: 30px;
+        background: white;
+        border: white;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.11);
+        margin-bottom: 20px;
+        margin-right: 10px;
+        cursor: pointer;
+            
+    }
+`
+
--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,5 +1,5 @@
 import { Container } from "../styles/generalStyles/container.styled"
-import { BackButton, CountryDetails, OtherInfo } from "./details.styled"
+import { BackButton, BorderCountries, CountryDetails, CountryDetailsText, InfoContainer, OtherInfo } from "./details.styled"
 import back from '../../assets/left-arrow.png'
 import { useNavigate, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
@@ -11,6 +11,8 @@ const Details = () => {
     let {name} = useParams<{name: string}>()
 
     const [countryDetails, setCountryDetails] = useState('')
+
+    
 
     const fetchCountry = async () => {
         const response = await fetch(`https://restcountries.com/v2/name/${name}`)
@@ -25,13 +27,13 @@ const Details = () => {
         .then((response)=>{
             setCountryDetails(response[0])
         })
-    },[])
+    },[name])
 
     console.log(countryDetails)
 
     return(
         <Container>
-            <BackButton onClick={()=>navigate('/')}>
+            <BackButton onClick={() => navigate(-1)}>
                 <img src={back} alt='back-arrow'/>
                 Back
             </BackButton>
@@ -39,35 +41,66 @@ const Details = () => {
                 
                 <div>
                     {/* @ts-ignore */}
-                    <img src={countryDetails.flag} alt='flag'/>
+                    <img src={countryDetails?.flag} alt='flag'/>
                 </div>
-                <div>
+                <CountryDetailsText>
                     <CountryName>
                         {/* @ts-ignore */}
-                        {countryDetails.name}
+                        {countryDetails?.name}
                     </CountryName>
-                    {/* @ts-ignore */}
-                    <p>Native Name: <span>{countryDetails.nativeName}</span>{}</p>
-                    {/* @ts-ignore */}
-                    <p>Population: <span>{countryDetails.population}</span>{}</p>
-                    {/* @ts-ignore */}
-                    <p>Region: <span>{}</span>{countryDetails.region}</p>
-                    {/* @ts-ignore */}
-                    <p>Sub Region: <span>{countryDetails.subregion}</span>{}</p>
-                    {/* @ts-ignore */}
-                    <p>Capital: <span>{}</span>{countryDetails.capital}</p>
-                    
 
-                    <OtherInfo>
-                        {/* @ts-ignore */}
-                        <p>Top Level Domain: <span>{countryDetails.topLevelDomain}</span></p>
-                        {/* @ts-ignore */}
-                        {/* <p>Currencies: <span>{countryDetails.currencies[0].code}</span></p> */}
-                        {/* @ts-ignore */}
-                        {/* <p>Languages: <span>{countryDetails.languages[0]}</span></p> */}
-                    </OtherInfo>
+                    <InfoContainer>
+                        <div>
+                                 {/* @ts-ignore */}
+                
+                            <p>Native Name: <span>{countryDetails?.nativeName}</span>{}</p>
+                            {/* @ts-ignore */}
+                            <p>Population: <span>{countryDetails?.population}</span>{}</p>
+                            {/* @ts-ignore */}
+                            <p>Region: <span>{}</span>{countryDetails?.region}</p>
+                            {/* @ts-ignore */}
+                            <p>Sub Region: <span>{countryDetails?.subregion}</span>{}</p>
+                            {/* @ts-ignore */}
+                            <p>Capital: <span>{}</span>{countryDetails?.capital}</p>
+                        
+                        </div>
+                             
+
+                        <OtherInfo>
+                            {/* @ts-ignore */}
+                            <p>Top Level Domain: <span>{countryDetails.topLevelDomain}</span></p>
+                            {/* @ts-ignore */}
+                            <p>Currencies: <span>{countryDetails?.currencies?.code}</span></p>
+                            {/* @ts-ignore */}
+                            <p>Languages: <span>{countryDetails.languages?.name}</span></p>
+                        </OtherInfo>
+
+
+                    </InfoContainer>
+              
+
+                    <BorderCountries>
+                        <p>Border Countries:</p>
+                        <div>
+                             
+                            {
+                                // @ts-ignore
+                                countryDetails.borders?.map((border)=>{
+                                    return(
+                                        <button 
+                                            key={border}
+                                            onClick={()=>navigate(`/${border}`)}
+                                        >
+                                            {border}
+                                        </button>
+                                    )
+                                })
+                            }
+                        </div>
+                        
+                    </BorderCountries>
                     
-                </div>
+                </CountryDetailsText>
                 
             </CountryDetails>
         </Container>
